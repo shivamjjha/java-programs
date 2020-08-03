4e8d16578ecc5a69ab5d13630e13a3f844813b65
shivamjjha <shivamjjha@gmail.com>
2020-08-03 20:40:30 +0530

    Employee class working finally

--- Employee.java
+++ Employee.java
@@ -8,43 +8,59 @@ public class Employee {
     String name;
     double salary;
 
-    void getData() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\tName: ");
-        name = scanner.nextLine();
+    void getData(Scanner scanner) {
+        // Scanner scanner = new Scanner(System.in);
+        System.out.print("\tName: \n\t");
+        name = scanner.next();
 
-        System.out.println("\tSalary: ");
+        System.out.print("\tSalary: \n\t");
         salary = scanner.nextDouble();
-
-        scanner.close();
     }
 
     void printData() {
         System.out.println("\tName: " + name);
-        System.out.println("\tName: " + salary);
+        System.out.println("\tName: " + salary + '\n');
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         try {
             System.out.println("Enter number of employees: ");
             int num = scanner.nextInt();
 
-            Employee[] employees = new Employee[3];
-            System.out.println(employees.length);
+            Employee[] employees = new Employee[num];
 
             for (int i = 0; i < employees.length; i++) {
-                employees[i].getData();
+                employees[i] = new Employee();
+            }
+            int i = 0;
+            // System.out.println(employees.length);
+
+            /*
+             * employees[0].getData(); employees[1].getData();
+             */
+
+            for (i = 0; i < employees.length; i++) {
+                employees[i].getData(scanner);
             }
 
-            for (int i = 0; i < employees.length; i++) {
+            /*
+             * for (int i = 0; i < employees.length; i++) { employees[i].printData(); }
+             */
+
+            /*
+             * System.out.println("Just before loop"); int i = 0; while (num > i &&
+             * scanner.hasNextLine()) { System.out.println("In loop");
+             * System.out.print("\tName: \n\t"); employees[i].name = scanner.next();
+             * 
+             * System.out.print("\tSalary: \n\t"); employees[i].salary =
+             * scanner.nextDouble(); ++i; }
+             */
+            System.out.println("\n-----Employee Data-----");
+            for (i = 0; i < employees.length; i++) {
                 employees[i].printData();
             }
-            /* Employee a = new Employee();
-            a.getData();
-            a.printData(); */
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
