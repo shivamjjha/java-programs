7fc9b2cd905ba7be67d16cd3bef5a9bd24c11c1b
palak199 <p.thapar99@gmail.com>
2020-10-03 06:29:44 +0530

    fixed a bug

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
  * 
  * Author: Shivam Jha
  */
+
 public class SumOfDigits {
+
+    public static int sum(int a){
+        if(a==0)
+        return a;
+        else return a%10+sum(a/10);
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.println("Enter two numbers:");
+            System.out.println("Enter a number:");
 
             int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            System.out.println("The sum of numbers entered is: " + a + b);
+            
+            System.out.println("The sum of digits of the number entered is: " + sum(a));
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println("\n---Please enter valid numbers---\n");
+            System.err.println("\n---Please enter valid number---\n");
         } finally {
             scanner.close();
         }
